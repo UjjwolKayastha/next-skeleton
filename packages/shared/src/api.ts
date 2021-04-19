e4cdb725183611a250e18817d97504bd57fc3509
Ujjwol Kayastha <uzol123@gmail.com>
2021-04-19 16:07:42 +0545

    feat: react query and axios setup

---
+++ packages/shared/src/api.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+import { API_URL } from "./config";
+import { handleError } from "./lib";
+
+axios.defaults.baseURL = API_URL;
+axios.defaults.headers["Content-Type"] = "application/json";
+axios.defaults.responseType = "json";
+
+const API = axios.create();
+
+API.interceptors.request.use(
+  async (cfg) => {
+    const token = "token";
+
+    cfg.headers["Authorization"] = "Bearer " + token;
+
+    return cfg;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+API.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (err) => {
+    if (
+      err.response &&
+      (err.response.status === 401 || err.response.status === 403)
+    ) {
+    }
+    return Promise.reject(handleError(err));
+  }
+);
+
+export { API };
--- packages/user/pages/_app.tsx
+++ packages/user/pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { QueryClientProvider, QueryClient } from "react-query";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const queryClient = new QueryClient();
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
+};
+
+export default MyApp;
