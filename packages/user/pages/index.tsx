ef43e6df936dd3cfd363431a7ce172712c364322
Ujjwol Kayastha <uzol123@gmail.com>
2021-04-19 16:10:06 +0545

    feat: sample implementation

--- packages/user/pages/index.tsx
+++ packages/user/pages/index.tsx
@@ -1,10 +1,20 @@
+import { useQuery } from "react-query";
 import { Button } from "shared";
+import { movieService } from "shared";
 
 export default function Home() {
+  const { data: popularMovies } = useQuery(
+    ["getPopularMovies"],
+    movieService.getPopularMovies
+  );
+
   return (
-    <div>
-      HI THERE
+    <>
       <Button />
-    </div>
+      {popularMovies &&
+        popularMovies?.results?.map((popularMovie) => {
+          return <p key={popularMovie.id}>{popularMovie.original_title}</p>;
+        })}
+    </>
   );
 }
