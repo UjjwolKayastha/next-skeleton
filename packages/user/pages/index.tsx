c0193cac61d39c729addb37faf702dccafc9fe9c
Ujjwol Kayastha <uzol123@gmail.com>
2021-04-20 17:22:04 +0545

    feat: sample implementation

--- packages/user/pages/index.tsx
+++ packages/user/pages/index.tsx
@@ -1,20 +1,29 @@
 import { useQuery } from "react-query";
-import { Button } from "shared";
-import { movieService } from "shared";
+import { Card } from "semantic-ui-react";
+import { movieService, MovieCard } from "shared";
 
 export default function Home() {
-  const { data: popularMovies } = useQuery(
+  const { data: popularMovies, isLoading } = useQuery(
     ["getPopularMovies"],
     movieService.getPopularMovies
   );
 
   return (
     <>
-      <Button />
-      {popularMovies &&
-        popularMovies?.results?.map((popularMovie) => {
-          return <p key={popularMovie.id}>{popularMovie.original_title}</p>;
+      <Card.Group centered>
+        {popularMovies?.results?.map((popularMovie) => {
+          return (
+            <MovieCard
+              isLoading={isLoading}
+              original_title={popularMovie.original_title}
+              backdrop_path={popularMovie.backdrop_path}
+              overview={popularMovie.overview}
+              vote_average={popularMovie.vote_average}
+              release_date={popularMovie.release_date}
+            />
+          );
         })}
+      </Card.Group>
     </>
   );
 }
