e2be03bfae8aeebb48b08c40cfe0cf6ccde58478
Ujjwol Kayastha <uzol123@gmail.com>
2021-04-20 17:20:14 +0545

    feat: update interfaces and add config to shared

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,3 +3,4 @@ export * from "./api";
 export * from "./lib";
 export * from "./services";
 export * from "./interfaces";
+export * from "./config";
--- packages/shared/src/interfaces/movie.ts
+++ packages/shared/src/interfaces/movie.ts
@@ -1,21 +1,21 @@
 export interface Movie {
-    adult?: boolean;
-    original_title: string;
-    title: string;
-    vote_average: number;
-    original_language: string;
-    genre: string | string[];
-    id: number;
-    overview: string;
-    poster_path: string;
-    backdrop_path: string;
-    vote_count: number;
-  }
-  
-  export interface IPopularMovies {
-    page: number;
-    results: Movie[];
-    total_pages: number;
-    total_results: number;
-  }
-  
+  adult?: boolean;
+  original_title: string;
+  title?: string;
+  vote_average: number;
+  original_language?: string;
+  genre?: string | string[];
+  id?: number;
+  overview: string;
+  poster_path?: string;
+  backdrop_path: string;
+  vote_count?: number;
+  release_date?: string;
+}
+
+export interface IPopularMovies {
+  page?: number;
+  results?: Movie[];
+  total_pages?: number;
+  total_results?: number;
+}
