05c1aaaa53b414c32065d749e61cd4355d15019f
Ujjwol Kayastha <uzol123@gmail.com>
2021-04-21 14:49:59 +0545

    feat: add home cover to index page

--- packages/user/pages/index.tsx
+++ packages/user/pages/index.tsx
@@ -1,6 +1,13 @@
 import { useQuery } from "react-query";
-import { Card } from "semantic-ui-react";
-import { movieService, MovieCard } from "shared";
+import { Card, Image } from "semantic-ui-react";
+import {
+  movieService,
+  MovieCard,
+  IMAGE_BASE_URL,
+  BACKDROP_SIZE,
+  HomeCover,
+  GlobalStyle,
+} from "shared";
 
 export default function Home() {
   const { data: popularMovies, isLoading } = useQuery(
@@ -10,13 +17,21 @@ export default function Home() {
 
   return (
     <>
+      {popularMovies && (
+        <HomeCover
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${popularMovies.results[0].backdrop_path}`}
+          original_title={popularMovies.results[0].original_title}
+          overview={popularMovies.results[0].overview}
+        />
+      )}
+
       <Card.Group centered>
         {popularMovies?.results?.map((popularMovie) => {
           return (
             <MovieCard
               isLoading={isLoading}
               original_title={popularMovie.original_title}
-              backdrop_path={popularMovie.backdrop_path}
+              poster_path={popularMovie.poster_path}
               overview={popularMovie.overview}
               vote_average={popularMovie.vote_average}
               release_date={popularMovie.release_date}
