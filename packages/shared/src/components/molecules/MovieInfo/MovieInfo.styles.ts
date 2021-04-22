4031e119fd3a3d7b934e35a4a1a94114c1368545
Ujjwol Kayastha <uzol123@gmail.com>
2021-04-22 16:14:20 +0545

    feat: add movie info

---
+++ packages/shared/src/components/molecules/MovieInfo/MovieInfo.styles.ts
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+import { IMAGE_BASE_URL, BACKDROP_SIZE } from "../../config";
+
+export const Wrapper = styled.div`
+  background: ${({ backdrop }) =>
+    backdrop ? `url(${IMAGE_BASE_URL}${BACKDROP_SIZE}${backdrop})` : "#000"};
+  background-size: cover;
+  background-position: center;
+  padding: 40px 20px;
+  animation: animateMovieInfo 1s;
+
+  @keyframes animateMovieInfo {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+export const Content = styled.div`
+  display: flex;
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  background: rgba(0, 0, 0, 0.7);
+  border-radius: 10px;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    max-height: none;
+  }
+`;
+
+export const Text = styled.div`
+  width: 100%;
+  padding: 20px 40px;
+  color: var(--white);
+  overflow: hidden;
+
+  .rating-directors {
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  .rating {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 35px;
+    height: 35px;
+    background: black;
+    border-radius: 50%;
+    font-weight: 900;
+  }
+
+  .director {
+    margin: 0 0 0 20px;
+    p {
+      margin: 0;
+    }
+  }
+
+  h1 {
+    @media screen and (max-width: 768px) {
+      font-size: var(--fontBig);
+    }
+  }
+`;
---
+++ packages/shared/src/components/molecules/MovieInfo/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+//Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../../../config";
+
+//Styles
+import { Wrapper, Content, Text } from "./MovieInfo.styles";
+import { Movie, Credits } from "../../../interfaces";
+import { HomeCover } from "../HomeCover/index";
+
+export const MovieInfo: React.FC<Movie & Credits> = ({
+  backdrop_path,
+  poster_path,
+  title,
+  overview,
+  vote_average,
+  directors,
+}) => (
+  <Wrapper backdrop={backdrop_path}>
+    <Content>
+      <HomeCover image={`${IMAGE_BASE_URL}${POSTER_SIZE}${poster_path}`} />
+      <Text>
+        <h1>{title}</h1>
+        <h3>PLOT</h3>
+        <p>{overview}</p>
+
+        <div className="rating-directors">
+          <div>
+            <h3>RATING</h3>
+            <div className="rating">{vote_average}</div>
+          </div>
+          <div className="director">
+            {directors && <h3>DIRECTOR{directors?.length > 1 ? "S" : ""}</h3>}
+            {directors?.map((director) => (
+              <p key={director.credit_id}>{director.name}</p>
+            ))}
+          </div>
+        </div>
+      </Text>
+    </Content>
+  </Wrapper>
+);
