5f388f0cff23799d845c3fd512177655d6a53200
Ujjwol Kayastha <uzol123@gmail.com>
2021-04-23 11:55:46 +0545

    feat: update and add interfaces

--- packages/shared/src/interfaces/movie.ts
+++ packages/shared/src/interfaces/movie.ts
@@ -1,16 +1,57 @@
 export interface Movie {
   adult?: boolean;
-  original_title: string;
+  original_title?: string;
   title?: string;
-  vote_average: number;
+  vote_average?: number;
   original_language?: string;
   genre?: string | string[];
   id?: number;
-  overview: string;
+  overview?: string;
   poster_path?: string;
   backdrop_path?: string;
   vote_count?: number;
   release_date?: string;
+  runtime?: number;
+  budget?: number;
+  spoken_languages?: [
+    {
+      name: string;
+    }
+  ];
+  revenue?: number;
+  directors?: Crew[];
+}
+
+interface Cast {
+  cast_id?: number;
+  character?: string;
+  credit_id?: string;
+  gender?: number;
+  original_name?: string;
+  profile_path?: string;
+  id?: number;
+  known_for_department?: string;
+  adult?: boolean;
+  name?: string;
+}
+
+interface Crew {
+  cast_id?: number;
+  department?: string;
+  credit_id?: string;
+  gender?: number;
+  adult?: boolean;
+  original_name?: string;
+  profile_path?: string;
+  id?: number;
+  known_for_departmen?: string;
+  job?: string;
+  name?: string;
+}
+export interface Credits {
+  cast?: Cast[];
+  crew?: Crew[];
+  id?: number;
 }
 
 export interface IPopularMovies {
