5c410cdeac666e20d42849a20f5d69783a545baa
Ujjwol Kayastha <uzol123@gmail.com>
2021-04-23 22:10:40 +0545

    feat: add next-env.d.ts for svg import fixes

--- packages/user/next-env.d.ts
+++ packages/user/next-env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+/// <reference types="next-images" />
+
+declare module "*.svg" {
+  import React from "react";
+  export default JSX.IntrinsicElements.svg;
+}
