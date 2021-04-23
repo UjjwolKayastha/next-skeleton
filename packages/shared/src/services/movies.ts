3633dcbc05ad616efeda60c277eea6f86ef3b257
Ujjwol Kayastha <uzol123@gmail.com>
2021-04-23 11:56:29 +0545

    feat: update get movie by id service

--- packages/shared/src/services/movies.ts
+++ packages/shared/src/services/movies.ts
@@ -1,7 +1,7 @@
 import { API } from "../api";
 
 import { API_URL, POPULAR_BASE_URL, API_KEY } from "../config";
-import { IPopularMovies } from "../interfaces/movie";
+import { Credits, IPopularMovies, Movie } from "../interfaces/movie";
 
 const getPopularMovies = async () => {
   const response: IPopularMovies = await API.get(POPULAR_BASE_URL);
@@ -10,26 +10,60 @@ const getPopularMovies = async () => {
   }
 };
 
-const getMovieById = async (movieId: number) => {
-  const response = await API.get(
+const getMovieById = async (
+  movieId: number
+): Promise<Movie & Credits | undefined> => {
+  const movieResponse = (await API.get(
     `${API_URL}movie/${movieId}?api_key=${API_KEY}`
-  );
-  if (response) {
-    return response;
-  }
-};
+  )) as Movie;
 
-const getMovieCredits = async (movieId: number) => {
-  const response = await API.get(
+
+  const creditResponse = (await API.get(
     `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`
+  )) as Credits;
+
+  const {
+    backdrop_path,
+    id,
+    original_title,
+    vote_average,
+    title,
+    release_date,
+    poster_path,
+    runtime,
+    budget,
+    revenue,
+    spoken_languages,
+    overview,
+  } = movieResponse;
+
+  const directors = creditResponse.crew?.filter(
+    (member) => member.job === "Director"
   );
-  if (response) {
-    return response;
+
+  if (movieResponse && creditResponse) {
+    return {
+      backdrop_path,
+      id,
+      original_title,
+      vote_average,
+      title,
+      release_date,
+      poster_path,
+      crew: directors,
+      cast: creditResponse.cast,
+      runtime,
+      budget,
+      revenue,
+      spoken_languages,
+      overview,
+    };
+  } else {
+    return undefined;
   }
 };
 
 export const movieService = {
   getPopularMovies,
   getMovieById,
-  getMovieCredits,
 };
