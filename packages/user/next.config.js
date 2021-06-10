d3132145d9724c598d89a61f2718922ac051d7fb
Ujjwol Kayastha <uzol123@gmail.com>
2021-06-10 18:09:01 +0545

    feat: update next config to integrate sentry config

--- packages/user/next.config.js
+++ packages/user/next.config.js
@@ -2,11 +2,22 @@ const withPlugins = require("next-compose-plugins");
 const withTM = require("next-transpile-modules")(["shared"]);
 const withImages = require("next-images");
 
-module.exports = withPlugins([withTM(), withImages], {
-  webpack: (config) => {
-    // custom webpack config
-    return config;
-  },
-  distDir:"../../out",
-  images: {},
-});
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const SentryWebpackPluginOptions = {};
+
+module.exports = withPlugins(
+  [
+    withTM(),
+    withImages,
+    (nextConfig) => withSentryConfig(nextConfig, SentryWebpackPluginOptions),
+  ],
+  {
+    webpack: (config) => {
+      // custom webpack config
+      return config;
+    },
+    distDir: "../../out",
+    images: {},
+  }
+);
