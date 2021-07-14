2c79717cac8900024972d307a3abadec3f061efb
UjjwolKayastha <uzol123@gmail.com>
2021-07-14 18:34:41 +0545

    feat: update babel and next configs

--- packages/admin/next.config.js
+++ packages/admin/next.config.js
@@ -1,10 +1,36 @@
-const withPlugins = require("next-compose-plugins");
 const withTM = require("next-transpile-modules")(["shared"]);
 const withImages = require("next-images");
-module.exports = withPlugins([withTM(), withImages], {
-  webpack: (config) => {
-    // custom webpack config
-    return config;
-  },
-  images: {},
-});
+const nextSourceMaps = require("@zeit/next-source-maps");
+const withLinaria = require("next-linaria");
+const withAntdLess = require("next-plugin-antd-less");
+
+module.exports = withImages(
+  nextSourceMaps(
+    withTM(
+      withLinaria(
+        withAntdLess({
+          webpack: (config, { isServer }) => {
+            if (!isServer) {
+              config.resolve.alias["@sentry/node"] = "@sentry/browser";
+            }
+            // work around to tell linaria to compile shared package
+            config.module.rules.push({
+              test: /\.([jt]sx?|mjs)$/,
+              use: [
+                {
+                  loader: "linaria/loader",
+                  options: {
+                    extension: ".linaria.module.css",
+                    evaluate: true,
+                    ignore: /node_modules\/(?!@project\/shared)/,
+                  },
+                },
+              ],
+            });
+            return config;
+          },
+        })
+      )
+    )
+  )
+);
